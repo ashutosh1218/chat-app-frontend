e069603b1389f3d74efddb630320a450232f3817
Ashutosh Kumar Singh <84225421+ashutosh1218@users.noreply.github.com>
2022-07-09 18:41:10 +0530

    Changed backend URL

--- src/utils/APIRoutes.jsx
+++ src/utils/APIRoutes.jsx
@@ -1,4 +1,5 @@
-export const host="http://localhost:5000";
+//export const host="http://localhost:5000";
+export const host="https://enigmatic-wildwood-53719.herokuapp.com";
 
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
