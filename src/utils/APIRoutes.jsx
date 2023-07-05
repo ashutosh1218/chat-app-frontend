1eb3bdbe1b217285b01447cef242350c17d820f0
Ashutosh Kumar Singh <84225421+ashutosh1218@users.noreply.github.com>
2023-07-05 14:38:19 +0530

    updated backend url

--- src/utils/APIRoutes.jsx
+++ src/utils/APIRoutes.jsx
@@ -1,5 +1,5 @@
 //export const host="http://localhost:5000";
-export const host="https://enigmatic-wildwood-53719.herokuapp.com";
+export const host="https://chatappbackend-czyj.onrender.com";
 
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
